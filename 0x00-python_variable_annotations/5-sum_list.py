4ad3f380c3eb4eb6b911d6c774e8640f5f761bc5
doni-robert <irobertndungu@gmail.com>
2023-10-06 05:14:19 +0300

    use typing.List

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Contains function sum_list """
-from typing import Iterable
+from typing import List 
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Adds up the floats from a list
 
