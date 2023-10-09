4bf6a1afac9fb5a48177fe23554635a5bf017e28
doni-robert <irobertndungu@gmail.com>
2023-10-09 13:23:21 +0300

    task 1 done

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Contains the 'wait_n' function """
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    """
+    Spawns 'wait_random' n times with the specified 'max_delay'
+
+    Args:
+        n(int): number of times to spawn
+        max_delay(int): upper limit of the random delay time
+
+    Return:
+        a list of all the delays in ascending order
+    """
+    delay_list: list[float] = []
+    for i in range(n):
+        delay_list.append(await wait_random(max_delay))
+
+    return sorted(delay_list)
