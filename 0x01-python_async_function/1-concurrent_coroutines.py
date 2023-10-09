6442d267ebb1e6eddac402b994888c021a25ada0
doni-robert <irobertndungu@gmail.com>
2023-10-09 13:24:50 +0300

    correct type assertion

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ Contains the 'wait_n' function """
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns 'wait_random' n times with the specified 'max_delay'
 
@@ -16,7 +17,7 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     Return:
         a list of all the delays in ascending order
     """
-    delay_list: list[float] = []
+    delay_list: List[float] = []
     for i in range(n):
         delay_list.append(await wait_random(max_delay))
 
